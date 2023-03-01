7287eee2a98cdc692186dfba7c9ae402750109ae
Martin Vig <102515191+Nokkenandre@users.noreply.github.com>
2023-03-01 14:14:15 +0100

    Update passord.py

--- passord.py
+++ passord.py
@@ -1,2 +1,3 @@
+# Lagrer passordet mitt vekke fra hovedkoden for å ungå å vise det med et uheld. Passordet og brukernavnet er fjernet for åpenbare grunner
 password = "Passord"
 username = "Brukernavn"
